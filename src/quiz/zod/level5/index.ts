37f73dbf17f0e66de77dd6dc339bf97f0cd597d9
y-natani <yn1323@gmail.com>
2024-09-29 21:56:52 +0900

    feat: level5

---
+++ src/quiz/zod/level5/index.ts
@@ -0,0 +1,25 @@
+/**
+ * ====解説====
+ * UIをベースにスキーマをつくってみよう
+ *
+ * これまでの知識をもとにUI、仕様をベースにスキーマをつくってみましょう
+ * また、作成したスキーマにテストを書いてみましょう
+ *
+ * ====問題====
+ * 下記フォームのスキーマを作成、テストコードを書いてみましょう
+ * https://github.com/user-attachments/assets/a3c17053-9f53-4f4c-ba80-77181e83b280
+ *
+ * 想定するエラー、エラーメッセージは自由に設定してください
+ * `npm run test zod/level5`ですべてのテストが通ることを確認しよう
+ *
+ * ====ヒント====
+ * https://zod.dev
+ */
+
+import z from 'zod';
+
+export const b1 = () => {
+  const schema = z.xxxx();
+
+  return schema;
+};
